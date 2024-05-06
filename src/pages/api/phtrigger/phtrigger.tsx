5c18f6e8ee54071f1348b7edace11856663437da
cbawebdesign <35353826+cbawebdesign@users.noreply.github.com>
2024-05-06 16:11:04 -0700

    Create PHTrigger

---
+++ src/pages/api/phtrigger/phtrigger.tsx
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        try {
+            // Use the URL of your Google Cloud Function
+            const cloudFunctionUrl = 'https://us-central1-amidnight1a.cloudfunctions.net/premium_history ';
+
+            const response = await fetch(cloudFunctionUrl, { method: 'POST' });
+            const data = await response.text();
+
+            // Return the response from the Cloud Function
+            res.status(200).json({ message: 'Python script executed', data });
+        } catch (error) {
+            console.error('Error running Python script:', (error as Error).message);
+            res.status(500).json({ error: 'Internal Server Error', details: (error as Error).message });
+        }
+    } else {
+        res.status(405).end('Method Not Allowed');
+    }
+}
