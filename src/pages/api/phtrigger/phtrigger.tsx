49907afcd1f3de86ed972ee998cd3ad09c301a36
cbawebdesign <35353826+cbawebdesign@users.noreply.github.com>
2024-05-06 20:50:25 -0700

    comit

--- src/pages/api/phtrigger/phtrigger.tsx
+++ src/pages/api/phtrigger/phtrigger.tsx
@@ -7,10 +7,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const cloudFunctionUrl = 'https://us-central1-amidnight1a.cloudfunctions.net/premium_history ';
 
             const response = await fetch(cloudFunctionUrl, { method: 'POST' });
-            const data = await response.text();
+            const data = await response.json(); // Use json() instead of text()
 
-            // Return the response from the Cloud Function
-            res.status(200).json({ message: 'Python script executed', data });
+            // Check if the Cloud Function is still processing
+            if (data.status === 'Processing') {
+                // Return a 202 status to indicate that the request has been accepted but is not yet complete
+                res.status(202).json({ message: 'Processing', data });
+            } else {
+                // Return the response from the Cloud Function
+                res.status(200).json({ message: 'Python script executed', data });
+            }
         } catch (error) {
             console.error('Error running Python script:', (error as Error).message);
             res.status(500).json({ error: 'Internal Server Error', details: (error as Error).message });
